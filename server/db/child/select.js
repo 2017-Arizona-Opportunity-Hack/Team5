03b190da6d0980bf9b6c93191bf94fbeb934e2be
James Quigley <James.A.Quigley@asu.edu>
2017-10-21 16:14:39 -0700

    Child Select Queries

---
+++ server/db/child/select.js
@@ -0,0 +1,16 @@
+import executeQuery from '../query';
+
+var mysql = require('mysql');
+
+export default {
+    byId: async(id) => {
+        var sql = "SELECT * FROM Child WHERE id=?";
+        sql = mysql.format(sql, [id]);
+        return executeQuery(sql);
+    },
+
+    all: async() => {
+        var sql = "SELECT * FROM Child";
+        return executeQuery(sql);
+    }
+};
