eefd27978985a78999b2bbcc8071457d2214a396
Josh Hewlett <jhewlett@asu.edu>
2017-10-21 14:10:57 -0700

    .jsbeautify file mods

--- server/server.js
+++ server/server.js
@@ -5,3 +5,10 @@ const HTTPStatusCodes = require('http-status-codes');
 let http = require('http');
 let https = require('https');
 let LocalStrategy = require('passport-local');
+
+import {
+  SessionConfig
+} from "./config/session.config"
+import {
+  AuthConfig
+} from "./config/auth.config"
