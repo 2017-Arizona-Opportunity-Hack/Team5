54bfc90ed2c2d605712d7034695d10d95471a81d
Alireza <alireza.bahremand@outlook.com>
2017-10-21 14:18:39 -0700

    Integration of Materialize
    Integration of Materialize CSS

---
+++ parentclient/src/components/Parent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+    <div>
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'parentview',
+  data () {
+    return {
+      msg: 'Welcome to Your Sunshine Acres'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- parentclient/src/router/index.js
+++ parentclient/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import parentview from '@/components/Parent'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'parentview',
+      component: parentview
     }
   ]
 })
