438a5c96bf62f4b1745c1673aba2fa836e640a36
Josh Hewlett <jhewlett@asu.edu>
2017-10-21 18:39:24 -0700

    Current state

--- server/controllers/base.controller.js
+++ server/controllers/base.controller.js
@@ -4,12 +4,19 @@ const HttpStatus = require('http-status-codes');
 import {
   JsonResponse
 } from "../models/jsonResponse.model";
+// import {
+//   DB
+// } from "../db";
 
 export class BaseController {
   constructor(server) {
     this.server = server;
     this.router = express.Router();
   }
+
+  // get DB() {
+  //   return this.DB;
+  // }
 
   sendResponse(res, status, success, data, message) {
     res.status(status);
--- server/controllers/index.controller.js
+++ server/controllers/index.controller.js
@@ -1,13 +1,30 @@
 import {
   BaseController
 } from "./base.controller";
+import DB from "../db/index";
 
 export class IndexController extends BaseController {
   constructor(server) {
     super(server);
 
     this.router.get('/', (req, res) => {
-      res.send("This is our home page. It is beautiful");
+      this.test(req, res);
     });
+  }
+
+  async test(req, res) {
+    try {
+
+      console.log("Hey")
+      let data = await DB.home.select.all().catch(err => {
+        console.log("Error: ", err);
+      });
+      console.log("hi");
+      console.log(data);
+      res.send(data);
+
+    } catch (err) {
+      console.log("Error ", err);
+    }
   }
 }
