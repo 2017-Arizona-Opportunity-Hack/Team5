a015ac8ab96d4ed8cff93a8424d359fccbbc9432
Josh Hewlett <jhewlett@asu.edu>
2017-10-22 11:34:04 -0700

    Reports for administration events

--- server/controllers/report.controller.js
+++ server/controllers/report.controller.js
@@ -15,8 +15,8 @@ export class ReportController extends BaseController {
     });
 
     // Retrieves a report by parent id and date
-    this.router.get("/byparentanddate/:parentid", (req, res) => {
-      this.getReportByParentAndDate(req, res).catch(err => {
+    this.router.get("/byparent/:parentid", (req, res) => {
+      this.getReportByParent(req, res).catch(err => {
         console.log("Error getting report by parent id and date: ", err);
         this.sendServerError(res, "Error getting report by parent id and date");
       });
@@ -32,17 +32,17 @@ export class ReportController extends BaseController {
 
     // Error handling
     this.router.all("/", (req, res) => {
-      res.status(HttpStatus.METHOD_NOT_ALLOWED);
+      res.status(this.HttpStatus.METHOD_NOT_ALLOWED);
       res.send("Error 405 - Method not allowed");
     });
 
     this.router.all("/:id", (req, res) => {
-      res.status(HttpStatus.METHOD_NOT_ALLOWED);
+      res.status(this.HttpStatus.METHOD_NOT_ALLOWED);
       res.send("Error 405 - Method not allowed");
     });
 
     this.router.get("/*", (req, res) => {
-      res.status(HttpStatus.NOT_FOUND);
+      res.status(this.HttpStatus.NOT_FOUND);
       res.send("Error 404 - Page not found")
     });
   }
@@ -53,27 +53,36 @@ export class ReportController extends BaseController {
     console.log(req.query);
     let data = {
       id: req.params.childid,
-      minDate: req.query.mindate
+      minDate: req.query.mindate,
+      maxDate: req.query.maxdate
     }
-    console.log(data);
 
-    let qData = await this.db.report.select.byChildIdAndDateRange(data).catch(this.throwError);
+    let qData = await this.db.report.select.byChildIdAndDateRange(data.id, data.minDate, date.maxDate).catch(this.throwError);
     this.sendResponse(res, this.HttpStatus.OK, true, qData, "Success retrieving report");
   }
 
-  // route: POST /
+  // route: GET /
   // Creates a new administration object
-  async getReportByParentAndDate(req, res) {
+  async getReportByParent(req, res) {
+    console.log(req.query);
+    let data = {
+      id: req.params.parentid
+    }
 
-
-    let qData = await this.db.administration.insert.one(data).catch(this.throwError);
-    this.sendResponse(res, this.HttpStatus.OK, true, qData, "Success retrieving all administrations");
+    let qData = await this.db.report.select.byParentId(data.id).catch(this.throwError);
+    this.sendResponse(res, this.HttpStatus.OK, true, qData, "Success retrieving report");
   }
 
   // route: GET /:id
   // Retrieves a administration object with the given id
   async getReportByDateRange(req, res) {
-    let data = await this.db.administration.select.byId(req.params.id).catch(this.throwError);
-    this.sendResponse(res, this.HttpStatus.OK, true, data, "Success administration");
+    console.log(req.query);
+    let data = {
+      minDate: req.query.mindate,
+      maxDate: req.query.maxdate
+    }
+
+    let qData = await this.db.report.select.byDateRange(data.minDate, data.maxDate).catch(this.throwError);
+    this.sendResponse(res, this.HttpStatus.OK, true, qData, "Success retrieving report");
   }
 }
--- server/db/report/select.js
+++ server/db/report/select.js
@@ -1,35 +1,47 @@
 import executeQuery from '../query';
 import {
-    DBConfig
+  DBConfig
 } from "../config";
 
 var mysql = require('mysql');
 var table = DBConfig.dbs.administration;
 
 export default {
-    byChildIdAndDateRange: async(id, minDate, maxDate) => {
-        if (!maxDate) {
-            maxDate = Date.now();
-        }
-        var sql = "SELECT * FROM " + table + " WHERE child_id=? AND date > ? AND date < ?";
-        sql = mysql.format(sql, [id, minDate, maxDate]);
-        return executeQuery(sql);
-    },
-    byDateRange: async(minDate, maxDate) => {
-        if (!maxDate) {
-            maxDate = Date.now();
-        }
-        var sql = "SELECT * FROM " + table + " WHERE date > ? AND date < ?";
-        sql = mysql.format(sql, [minDate, maxDate]);
-        return executeQuery(sql);
-    },
-    byParentId: async(id) => {
-        var sql = "SELECT * FROM " + table + " WHERE parent_id=?";
-        sql = mysql.format(sql, [id]);
-        return executeQuery(sql);
-    },
-    all: async() => {
-        var sql = "SELECT * FROM " + table;
-        return executeQuery(sql);
+  byChildIdAndDateRange: async(id, minDate, maxDate) => {
+    if (!maxDate) {
+      maxDate = new Date();
+    } else {
+      maxDate = new Date(parseInt(maxDate));
     }
+
+    minDate = new Date(parseInt(minDate)).toMysqlFormat();
+    maxDate = maxDate.toMysqlFormat();
+
+    var sql = "SELECT * FROM " + table + " WHERE child_id=? AND date > ? AND date < ?";
+    sql = mysql.format(sql, [id, minDate, maxDate]);
+    return executeQuery(sql);
+  },
+  byDateRange: async(minDate, maxDate) => {
+    if (!maxDate) {
+      maxDate = new Date();
+    } else {
+      maxDate = new Date(parseInt(maxDate));
+    }
+
+    minDate = new Date(parseInt(minDate)).toMysqlFormat();
+    maxDate = maxDate.toMysqlFormat();
+
+    var sql = "SELECT * FROM " + table + " WHERE date > ? AND date < ?";
+    sql = mysql.format(sql, [minDate, maxDate]);
+    return executeQuery(sql);
+  },
+  byParentId: async(id) => {
+    var sql = "SELECT * FROM " + table + " WHERE parent_id=?";
+    sql = mysql.format(sql, [id]);
+    return executeQuery(sql);
+  },
+  all: async() => {
+    var sql = "SELECT * FROM " + table;
+    return executeQuery(sql);
+  }
 };
