2fd146272c3682c151b0ec712f33ca6dd7deacf1
James Quigley <James.A.Quigley@asu.edu>
2017-10-22 09:50:45 -0700

    Default maxDate of now

--- server/db/custody/select.js
+++ server/db/custody/select.js
@@ -14,12 +14,18 @@ export default {
     },
 
     byChildIdAndDateRange: async(id, minDate, maxDate) => {
+        if (!maxDate) {
+            maxDate = Date.now();
+        }
         var sql = "SELECT * FROM " + table + " WHERE child_id=? AND date > ? AND date < ?";
         sql = mysql.format(sql, [id, minDate, maxDate]);
         return executeQuery(sql);
     },
 
     byDateRange: async(minDate, maxDate) => {
+        if (!maxDate) {
+            maxDate = Date.now();
+        }
         var sql = "SELECT * FROM " + table + " WHERE date > ? AND date < ?";
         sql = mysql.format(sql, [minDate, maxDate]);
         return executeQuery(sql);
