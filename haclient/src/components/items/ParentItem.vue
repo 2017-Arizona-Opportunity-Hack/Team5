b2c1d0a2dac7d3213bbe44115620ec20bb535bdb
Chuck Dries <chuck@chuckdries.com>
2017-10-22 09:03:08 -0700

    Parentitem

--- haclient/src/components/items/ParentItem.vue
+++ haclient/src/components/items/ParentItem.vue
@@ -4,17 +4,11 @@
         <div id="view" v-if="!editing">
             <div class="form-row align-items-center">
                 <div class="col col-sm-1">
-                    <input v-if="parent.id!=0" id="name" type="text" class="form-control-plaintext" v-model="parent.id" disabled>
+                    <input v-if="parent.id!=0" id="id" type="text" class="form-control-plaintext" v-model="parent.id" disabled>
                 </div>
-                <div class="col col">
+                <div class="col">
                     <input id="name" type="text" class="form-control" v-model="parent.name" disabled>
                 </div>
-                <div class="col col-sm-5">
-                    <input id="name" type="text" class="form-control" v-model="parent.home_id" disabled>
-                </div>
-                <!-- <button type="button" class="btn btn-block col-sm-1 btn-primary" @click="beginEdit">
-                            <i class="icon-pencil"></i>
-                        </button> -->
                 <a class="" href="#" @click="beginEdit">
                     <i class="icon-pencil"></i>
                 </a>
@@ -24,16 +18,12 @@
         <div id="edit" v-if="editing">
             <div class="form-row align-items-center">
                 <div class="form-group col col-sm-1">
-                    <label for="name">id</label>
-                    <input id="name" type="text" class="form-control-plaintext" v-model="editableParent.id" disabled>
+                    <label for="id">id</label>
+                    <input id="id" type="text" class="form-control-plaintext" v-model="editableParent.id" disabled>
                 </div>
                 <div class="form-group col">
                     <label for="name">name</label>
                     <input id="name" type="text" class="form-control" v-model="editableParent.name">
-                </div>
-                <div class="form-group col-sm-5">
-                    <label for="home">home id</label>
-                    <input id="name" type="text" class="form-control" v-model="editableParent.home_id">
                 </div>
                 <a href="#" class="text-danger" @click="cancelEdit">
                     <i class="icon-cancel"></i>
