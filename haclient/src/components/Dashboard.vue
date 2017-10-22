1ebdec2bc9461a42b30085d313a2331444b0fce6
Chuck Dries <chuck@chuckdries.com>
2017-10-22 12:15:54 -0700

    Multi-picker!

--- haclient/src/components/Dashboard.vue
+++ haclient/src/components/Dashboard.vue
@@ -2,6 +2,7 @@
     <div class="row">
         <div class="col">
             <h1 class="display-3">Welcome</h1>
+            <p>{{selectedChildren}}</p>
             Pick a table to enter data, or pick reports to generate reports.
             <picker collectionSource="children" multiple="true" v-model="selectedChildren"/>
         </div>
--- haclient/src/components/Picker.vue
+++ haclient/src/components/Picker.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="card">
-        <input type="text" name="id-output" id="id-output" :value="checkedItems">
-        <input type="text" name="name-output" id="name-output" :value="checkedNames">
-        <div class="list-group list-group-flush">
+    <div class="card" ref="customInput">
+        <input type="text" ref="displayer" @focus="focused=true" name="name-output" id="name-output" :value="checkedNames" @blur="stopBlur($event)">
+        <div v-if="focused" class="list-group list-group-flush">
             <div class="form-check list-group-item" v-for="item in collection" :key="item.id">
                 <label class="form-check-label">
                     <input type="checkbox" class="form-check-input" v-model="checkedItems" :value="item.id"> {{item.name}}
@@ -15,32 +14,44 @@
 <script lang="ts">
 export default {
 
-    data(){
+    data() {
         return {
-            checkedItems: this.value
+            checkedItems: this.value,
+            focused: false
         }
     },
     props: [
         "collectionSource", "multiple-select", "value"
     ],
     computed: {
-        collection: function() {
-            return this.$store.getters[this.collectionSource].map((item:any)=>(
-                {id: item.id, name:item.name}
+        collection: function () {
+            return this.$store.getters[this.collectionSource].map((item: any) => (
+                { id: item.id, name: item.name }
             ));
         },
-        checkedNames(){
-            return this.value.map((itemid: number)=>this.collection.filter((item: any)=>item.id==itemid).map((item: any)=>item.name));
+        checkedNames() {
+            return this.value.map((itemid: number) => this.collection.filter((item: any) => item.id == itemid).map((item: any) => " " + item.name));
         }
     },
-    watch:{
-        "checkedItems": function(){
+    watch: {
+        "checkedItems": function () {
             this.$emit('input', this.checkedItems);
         }
     },
     methods: {
-        updateValue(value: number){
+        stopBlur(event: any) {
+            var target = event.explicitOriginalTarget;
+            if (this.$refs.customInput.contains(target)) {
+                console.log("clicked in!")
+                this.$nextTick(() => {
 
+                    this.$refs.displayer.focus();
+                })
+                console.log(this.$refs.displayer)
+            } else {
+                console.log("clicked out!")
+                this.focused = false;
+            }
         }
     }
 }
